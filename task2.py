af650ab66986a74b44730e828f0e35a9d2fd6a94
Miguel Garcia Naude <miguelito2384@gmail.com>
2022-09-12 11:29:46 +0200

    Assignmnet complete?

--- task2.py
+++ task2.py
@@ -39,17 +39,17 @@ sim_env = EmbeddedSimEnvironment(model=bumble,
 t, y, u = sim_env.run(x0, x_ref=x_full_ref)
 
 # TODO: RUN FOR QUESTION 4
-# sim_env.visualize_error()
+sim_env.visualize_error()
 # # NOTE: You can further observe what the prediction for the solver is, versus the actual state
-# sim_env.visualize_prediction_vs_reference(x_pred=x_star, x_ref=x_full_ref, control=u_star)
-# sim_env.visualize_state_vs_reference(state=y, ref=x_full_ref, control=u_star)
+sim_env.visualize_prediction_vs_reference(x_pred=x_star, x_ref=x_full_ref, control=u_star)
+sim_env.visualize_state_vs_reference(state=y, ref=x_full_ref, control=u_star)
 
 
 # TODO: RUN FOR QUESTION 5 & 6
 # Activate broken thruster and re-run
-# sim_env.broken_thruster()
-# t, y, u = sim_env.run(x0, x_ref=x_full_ref)
-# sim_env.visualize_error()
+sim_env.broken_thruster()
+t, y, u = sim_env.run(x0, x_ref=x_full_ref)
+sim_env.visualize_error()
 
 # # TODO: RUN FOR QUESTION 7
 # Get the system discrete-time dynamics with Z
